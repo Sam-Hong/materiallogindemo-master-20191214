36512d2e965f8e4212b52f772f700772ddd4013f
Kristopher yang <kristopheryang1@gmail.com>
2020-01-08 09:08:21 +0800

    fixed the links

--- app/src/main/java/com/sourcey/materiallogindemo/StatsYearEight.java
+++ app/src/main/java/com/sourcey/materiallogindemo/StatsYearEight.java
@@ -38,32 +38,32 @@ public class StatsYearEight extends AppCompatActivity {
             public void onItemClick(AdapterView listView, View itemView, int position, long id) {
                 if (position==0){
                     Intent intent=new Intent(StatsYearEight.this,WebViewActivity.class);
-                    intent.putExtra("url","https://drive.google.com/file/d/1hW1I2XnOZc44rRM85wfIW7Vrl_ysO6IJ/preview");
+                    intent.putExtra("url","https://drive.google.com/file/d/1F014Dpaxfq_k5-6q-CVWIbZd6JQ3AwQv/preview");
                     startActivity(intent);
                 }
                 else if (position==1){
                     Intent intent=new Intent(StatsYearEight.this,WebViewActivity.class);
-                    intent.putExtra("url","https://drive.google.com/file/d/1GYcHnsLYclNsia_flHiluZ2ihSRovI1n/preview");
+                    intent.putExtra("url","https://drive.google.com/file/d/1YsYe4kCXEEQiDQcsHd18649U5vrEbMra/preview");
                     startActivity(intent);
                 }
                 else if (position==2){
                     Intent intent=new Intent(StatsYearEight.this,WebViewActivity.class);
-                    intent.putExtra("url","https://drive.google.com/file/d/1GYcHnsLYclNsia_flHiluZ2ihSRovI1n/preview");
+                    intent.putExtra("url","https://drive.google.com/file/d/1sjHqgXtTRB0Kj32_AQG0ZRcfPtvfG7cz/preview");
                     startActivity(intent);
                 }
                 else if (position==3){
                     Intent intent=new Intent(StatsYearEight.this,WebViewActivity.class);
-                    intent.putExtra("url","https://drive.google.com/file/d/1Ug35GfDPe1EVk4NhwoIzQeizc-OKQ5YL/preview");
+                    intent.putExtra("url","https://drive.google.com/file/d/1MHfZkpq5RJumtjYAoqys9dzoW69ZC8Jx/preview");
                     startActivity(intent);
                 }
                 else if (position==4){
                     Intent intent=new Intent(StatsYearEight.this,WebViewActivity.class);
-                    intent.putExtra("url","https://drive.google.com/file/d/1Ug35GfDPe1EVk4NhwoIzQeizc-OKQ5YL/preview");
+                    intent.putExtra("url","https://drive.google.com/file/d/1gvkmb7QqYyVwfjm8u0U_tqbG4Hzn3luy/preview");
                     startActivity(intent);
                 }
                 else if (position==5){
                     Intent intent=new Intent(StatsYearEight.this,WebViewActivity.class);
-                    intent.putExtra("url","https://drive.google.com/file/d/1Ug35GfDPe1EVk4NhwoIzQeizc-OKQ5YL/preview");
+                    intent.putExtra("url","https://drive.google.com/file/d/1IQP8Jgy6lyafhGonEvvLzu8lT5F5MZ6D/preview");
                     startActivity(intent);
                 }
                 else if (position==6){
@@ -73,7 +73,7 @@ public class StatsYearEight extends AppCompatActivity {
                 }
                 else if (position==7){
                     Intent intent=new Intent(StatsYearEight.this,WebViewActivity.class);
-                    intent.putExtra("url","https://drive.google.com/file/d/1rY6gS3YD8QWbFN6NOVSe6crg4O0FaY_J/preview");
+                    intent.putExtra("url","https://drive.google.com/file/d/13O4xWtJ13kI7eLinZfqE-ObjEIwWZNj_/preview");
                     startActivity(intent);
                 }
             }
